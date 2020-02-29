09ec24907bcf8b94679a7b1229fe8c239dac13c0
Pučmeloud <plenca@gmail.com>
2020-02-29 20:35:28 +0100

    Revert "Fungující kontaktní formulář jako podstránka. NUTNÉ dodat rechaptu!"
    This reverts commit e32a33111a4d9c53fead78b38835c26cafa37304.
    
    Conflicts:
    	kontrolery/KontaktKontroler.php
    	pohledy/kontakt.phtml

--- kontrolery/KontaktKontroler.php
+++ kontrolery/KontaktKontroler.php
@@ -1,31 +1,8 @@
 <?php
 
-class KontaktKontroler extends Kontroler {
-    public function zpracuj($parametry) {
-        $this->hlavicka = array (
-            'titulek' => 'Kontaktní formulář',
-            'klicova_slova' => 'kontakt, email, formulář',
-            'popis' => 'Kontaktní formulář našeho webu.',
-            'nadpis' => 'Kontaktní formulář'
-        );
-        
-        if (isset($_POST['submit'])) {
-            $email = $_POST['email'];
-            $secretKey = "6LeAft0UAAAAADlxCYJExbb0A9-SMkPHTwseqdaQ";
-            $responseKey = $_POST['g-recaptcha-response'];
-            $userIP = $_SERVER['REMOTE_ADDR'];
-            
-            $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$userIP";
-            $response = file_get_contents($url);
-            $response = json_decode($response);
-            
-            if ($response->success) {
-                $odesilacEmailu = new OdesilacEmailu();
-                $odesilacEmailu->odesli("plenca@gmail.com", $_POST['predmet'], $_POST['zprava'], $_POST['email']);
-            }
-            else 
-                echo '';
-        }
-        $this->pohled = 'kontakt';
-    }
-}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
