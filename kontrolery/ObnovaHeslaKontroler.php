49f25ba6d423e003e25d0aadd49cb02e28a90a02
Lencl <Lencl@LAPTOP-55C6LCAP.local>
2020-03-01 21:30:59 +0100

    Obnova hesla

--- kontrolery/ObnovaHeslaKontroler.php
+++ kontrolery/ObnovaHeslaKontroler.php
@@ -6,7 +6,7 @@ class ObnovaHeslaKontroler extends Kontroler {
             'titulek' => 'Obnova zapomenutého hesla',
             'klicova_slova' => 'uživatel, obnova, email, heslo',
             'popis' => 'Stránka sloužící k obnovení zapomenutého hesla.',
-            'nadpis' => 'Obonova hesla'
+            'nadpis' => 'Obnova hesla'
         );
         $this->pohled = 'obnova-hesla';
     }
