4c1087eaad97f5572255e19158d9842fe65e5a48
Pučmeloud <plenca@gmail.com>
2020-03-01 14:35:56 +0100

    Příprava pro stránku na obnovení hesla.

---
+++ kontrolery/ObnovaHeslaKontroler.php
@@ -0,0 +1,15 @@
+<?php
+
+class ObnovaHeslaKontroler extends Kontroler {
+    public function zpracuj($parametry) {
+        $this->hlavicka = array (
+            'titulek' => 'Obnova zapomenutého hesla',
+            'klicova_slova' => 'uživatel, obnova, email, heslo',
+            'popis' => 'Stránka sloužící k obnovení zapomenutého hesla.',
+            'nadpis' => 'Obonova hesla'
+        );
+        $this->pohled = 'obnova-hesla';
+    }
+}
+
+
